d53cf0264266437d5f34cd178ccd000457eb59ab
frank John <frankjoe261@gmail.com>
2022-12-07 20:36:50 +0100

    finished the category and category items components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Categories from "./components/categories/Categories.component";
+
+const directories = [
+  {id: 1, title: "Hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png"},
+  {id: 2, title: "Jackets", imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"},
+  {id: 3, title: "Snickers", imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"},
+  {id: 4, title: "Womens", imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"},
+  {id: 5, title: "Mens", imageUrl: "https://i.ibb.co/R70vBrQ/mens.png"},
+];
 
 const App = () => {
   return (
-    <div>Hello world</div>
+    <Categories directories={directories} />
   );
 }
 
