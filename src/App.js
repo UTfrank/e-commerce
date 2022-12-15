d7ca54e1642e79d7976055a81bd649fd707a546d
frank John <frankjoe261@gmail.com>
2022-12-15 13:25:02 +0100

    finished the sign up component and button component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import Categories from "./components/categories/Categories.component";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from "./routes/Home/Home.component";
+import Navigation from './routes/Navigation/Navigation.component';
+import SignIn from './routes/Sign-In/SignIn.component';
 
-const directories = [
-  {id: 1, title: "Hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png"},
-  {id: 2, title: "Jackets", imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"},
-  {id: 3, title: "Snickers", imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"},
-  {id: 4, title: "Womens", imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"},
-  {id: 5, title: "Mens", imageUrl: "https://i.ibb.co/R70vBrQ/mens.png"},
-];
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigation />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "sign-in",
+        element: <SignIn />
+      }
+    ]
+  }
+]);
 
 const App = () => {
-  return (
-    <Categories directories={directories} />
-  );
+  return(
+    <RouterProvider router={router} />
+  )
 }
 
 export default App;
