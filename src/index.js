64d3069b0bece1611ff4a79d126b45e65a8faa04
frank John <frankjoe261@gmail.com>
2023-01-20 13:51:23 +0100

    added the context for authentication and changed files with respect to it

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import { UserProvider } from './contexts/user.contexts';
 import reportWebVitals from './reportWebVitals';
 
 
@@ -9,7 +10,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
 
--- src/routes/Navigation/Navigation.component.jsx
+++ src/routes/Navigation/Navigation.component.jsx
@@ -1,10 +1,15 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { UserContext } from "../../contexts/user.contexts";
+
+import { signOutUser } from "../../utils/firebase/firebase.utils"
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss"
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <Fragment>
       <div className="navigation">
@@ -13,7 +18,13 @@ const Navigation = () => {
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">Shop</Link>
-          <Link className="nav-link" to="/auth">Sign In</Link>
+          {
+            currentUser ? (
+              <span className="nav-link" onClick={ signOutUser }>Sign Out</span>
+            ) : (
+              <Link className="nav-link" to="/auth">Sign In</Link>
+            )
+          }
         </div>
       </div>
       <Outlet />
